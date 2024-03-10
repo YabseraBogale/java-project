1f2be15756c9ea9b223d339fdc3d4004bdbaa555
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-03-10 13:25:51 +0300

    ok

---
+++ streams/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.Console;
+import java.io.FileInputStream;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // file needs to exits
+        FileInputStream fs=new FileInputStream("~/.idea/.gitignore");
+    }
+}
