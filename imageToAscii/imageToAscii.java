5e0a0fb890cfca2c8dbe583437772d324bc5f767
YabseraBogale <yabserapython@gmail.com>
2024-08-21 09:38:34 +0300

    ok

--- imageToAscii/imageToAscii.java
+++ imageToAscii/imageToAscii.java
@@ -1,10 +1,14 @@
 import java.io.File;
-import java.net.URI;
-
+import java.io.IOException;
 import javax.imageio.*;
 public class imageToAscii{
     public static void main(String[] args) {
+        try{
         var wall=new File("./wall.png");
-        System.out.println(wall.getName());
+        var bufferimage=ImageIO.read(wall);
+        System.err.println(bufferimage);
+        } catch(IOException e){
+            System.err.println(e.getMessage());
+        }
     }
 }
