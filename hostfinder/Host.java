33b92d8cba1eb1b51bf1b8ac16ba5dc5a108feba
YabseraBogale <yabserapython@gmail.com>
2024-08-21 09:39:06 +0300

    ok

--- hostfinder/Host.java
+++ hostfinder/Host.java
@@ -4,8 +4,11 @@ public class Host{
         try{
             String host=InetAddress.getByName("www.google.com").toString();
             System.out.println("google address: "+host.split("/")[1]);
+            String localhost=InetAddress.getLocalHost().toString();
+            System.out.println("local host: "+localhost);
+
         } catch(UnknownHostException e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
     }
 }
