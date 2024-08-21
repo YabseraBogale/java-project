5786b59d02ed98e4b8e944b017d96288c84355c0
YabseraBogale <yabserapython@gmail.com>
2024-08-21 09:36:03 +0300

    ok

---
+++ imageToAscii/imageToAscii.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.net.URI;
+
+import javax.imageio.*;
+public class imageToAscii{
+    public static void main(String[] args) {
+        var wall=new File("./wall.png");
+        System.out.println(wall.getName());
+    }
+}
