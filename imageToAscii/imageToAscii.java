06e8dbf6b9d88389abae8967c11317b4901c439b
YabseraBogale <yabserapython@gmail.com>
2024-08-21 10:33:06 +0300

    ok

--- imageToAscii/imageToAscii.java
+++ imageToAscii/imageToAscii.java
@@ -6,8 +6,14 @@ public class imageToAscii{
         try{
         var wall=new File("./wall.png");
         var bufferimage=ImageIO.read(wall);
-        System.err.println(bufferimage.getRGB(0, 0));
-        } catch(IOException e){
+        for(int i=0;i<bufferimage.getWidth();i++){
+            for(int j=0;j<bufferimage.getHeight();j++){
+                System.err.println(bufferimage.getRGB(j, i));
+            }
+        }
+        } catch(IOException e ){
+            System.err.println(e.getMessage());
+        } catch(ArrayIndexOutOfBoundsException e){
             System.err.println(e.getMessage());
         }
     }
